24408e5e889d7765e912a7df207a1d7dcc5f436f
amar chinta <amar.chinta@harbingergroup.com>
2020-12-27 18:25:26 +0530

    adding inheritance file

---
+++ learn_js/inheritance.js
@@ -0,0 +1,18 @@
+// Prototype is parent of all objects 
+
+// Root base class in javascript - ObjectBase
+
+let x = {};
+
+let y = {};
+
+// getting parent of x means ObjectBase
+ Object.getPrototypeOf(x) === Object.getPrototypeOf(y); // true
+
+
+//  let myArray = []
+
+// Multiple inheritance
+// ObjectBase => ArrayBase => myArray 
+
+
